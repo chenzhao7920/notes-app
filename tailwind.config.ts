9a33f889f129c8e211191453de7369da1aea3398
chenzhao7920 <chenzhao7920@gmail.com>
2024-12-14 13:01:04 -0500

    add Mui-ui package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,23 @@
 import type { Config } from "tailwindcss";
-
+import { nextui } from "@nextui-org/react";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
       colors: {
         background: "var(--background)",
@@ -14,5 +25,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
