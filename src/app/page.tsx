9bb216558264e29dd077fa82baf456b02f3a387e
chenzhao7920 <chenzhao7920@gmail.com>
2025-01-09 19:53:11 -0500

    test cors with deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
-import { getNotes  } from "@/api/notes.api"
+import axios from "axios";
+// import { getNotes  } from "@/api/notes.api"
 import NoteCreateForm from "@/app/components/note_create_form"
 import NoteList from "./components/note_list";
 export default function Home() {
@@ -9,8 +10,16 @@ export default function Home() {
   useEffect(() => {
     async function fetchNotes() {
       try {
-        const data = await getNotes();
-        setNotes(data);
+        const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/countries';
+        const config = {
+          headers: {
+            'x-rapidapi-key': '7676a93f26mshe6fc6f2f437c35ep1223e8jsnb36035426f98',
+            'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+          }
+        };
+        const {data: { data }} = await axios.get(url, config)
+        let notes = data?.map((e:any, i: number)=>({ _id: i, title: e?.code, content: e?.name }))
+        setNotes(notes);
       } catch (error) {
         console.error("Failed to fetch notes:", error);
         setNotes([]); // Fallback data
